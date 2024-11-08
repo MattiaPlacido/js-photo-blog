bd0aa32e009401c527e8b22d7ce8c30202faf490
MattiaPlacido <placido.mattia@gmail.com>
2024-11-08 13:13:24 +0100

    Concluso

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
 //FUNCTIONS
-const overlayOnClick = (card, overlayHandler) => {
-  card.addEventListener("click", () => {
-    if (overlayHandler) {
-      card.classList.remove("overlay");
-      overlayHandler = false;
-    } else {
+const overlayOnClick = (card, button) => {
+  //click iniziale sulla card
+  card.addEventListener("click", (e) => {
+    if (!card.classList.contains("overlay")) {
       card.classList.add("overlay");
-      overlayHandler = true;
+      button.classList.remove("d-none");
     }
   });
+  //click sul bottone per uscire
+  if (button.classList.contains("d-none")) {
+    button.addEventListener("click", (event) => {
+      button.classList.add("d-none");
+      card.classList.remove("overlay");
+    });
+  }
 };
 
 //HTML ELEMENTS
 const postsGroupEl = document.getElementById("posts-group");
 
 const postsNumber = 6;
-let isOverlayOn = false;
 
 //prendo i dati dall'api
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + postsNumber)
   .then((res) => res.json())
   .then((postsArray) => {
     //genero le cards in base ai dati presi
-    postsArray.forEach((post, index) => {
+    postsArray.forEach((post) => {
       postsGroupEl.innerHTML += `      
-        <div class="col">
+        <div class="col container-fluid mb-5">
         <div class="card post-card" id="${post.id}">
           <img class="pin" src="./img/pin.svg">
           <img class="post-image" src="${post.url}">
           <p>${post.title}</p>
         </div>
+        <button class="btn d-none border bg-white" type="button" id="overlay-exit${post.id}"><i class="fa-solid fa-x"></i></button>
       </div>`;
     });
     //gestisco l'overlay quando si clicca sulle carte
     const postCardsEl = document.querySelectorAll(".post-card");
-    postCardsEl.forEach((card) => {
-      overlayOnClick(card, isOverlayOn);
+    const postsButtons = [];
+    postCardsEl.forEach((card, index) => {
+      postsButtons[index] = document.getElementById(`overlay-exit${card.id}`);
+      overlayOnClick(card, postsButtons[index]);
     });
+  })
+  //gestisco l'eventualità non vada il servizio api
+  .catch((error) => {
+    console.log(error);
+    postsGroupEl.innerHTML += `
+      <div class="col-12">
+        <div class="alert alert-danger">
+          ${error}
+        </div>
+      </div>
+    `;
   });
